79810621c95bccff9887a477a9e3406db3d549fe
Juliana Midori Suda <jmidorisuda@gmail.com>
2014-04-01 21:33:14 -0300

    Update jokenpo_test.rb

--- jokenpo_test.rb
+++ jokenpo_test.rb
@@ -1,19 +1,33 @@
 
-def jogo(movimentos)
-	
-	if(movimentos.first == movimentos.last)
-		return "empate"
-	end 
-		
-	if(movimentos.include? "pedra") && (movimentos.include? "tesoura")
-		return "Vitoria pedra"
+require"./jokenpo"
+require "test/unit"
+
+
+class JokenpoTest < Test::Unit::TestCase
+				  	
+	def test_empate
+		movimentos = ["pedra", "pedra"]
+		assert_equal("empate", jogo(movimentos))
+		p "Resultado1: #{jogo(movimentos)}"
 	end
 
-	if(movimentos.include? "pedra") && (movimentos.include? "papel")
-		return "Vitoria papel"
-	end	
-
-	if(movimentos.include? "tesoura") && (movimentos.include? "papel")
-		return "Vitoria tesoura"
+	def test_pedra_tesoura
+		movimentos = ["pedra", "tesoura"]
+		assert_equal("Vitoria pedra", jogo(movimentos))
+		p "Resultado2: #{jogo(movimentos)}"
 	end
+
+	def test_pedra_papel
+		movimentos = ["pedra", "papel"]
+		assert_equal("Vitoria papel", jogo(movimentos))
+		p "Resultado3: #{jogo(movimentos)}"
+	end
+
+	def test_tesoura_papel
+		movimentos = ["tesoura", "papel"]
+		assert_equal("Vitoria tesoura", jogo(movimentos))
+		p "Resultado 4:#{jogo(movimentos)}"
+	end
+
+
 end
